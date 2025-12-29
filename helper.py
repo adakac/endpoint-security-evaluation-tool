b8e22b747920ccf7565ac09597e91a6793e621f9
is241836 <is241836@fhstp.ac.at>
2025-12-29 14:02:35 +0100

    Added functions for determining the previous and next change based on the alphabetical order. Also added more infos to each change.

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
 from table_definitions import *
-from sqlalchemy import select
+from sqlalchemy import select, asc, desc
 from sqlalchemy.orm import Session
 from requests import get
 from glom import glom
@@ -15,7 +15,15 @@ import json, re
 '''
 def parse_version_changes(from_version: str, to_version: str) -> list:
     result = []
-    changelog = get(f"https://attack.mitre.org/docs/changelogs/{from_version}-{to_version}/changelog.json")
+    try:
+        changelog = get(f"https://attack.mitre.org/docs/changelogs/{from_version}-{to_version}/changelog.json", timeout=10)
+        changelog.raise_for_status()
+        attack_data = get(f"https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack-{to_version[1:]}.json", timeout=10)
+        attack_data.raise_for_status()
+        attack_data = attack_data.json()
+    except:
+        print("TIMEOUT")
+        return
 
     # A JSON changelog is only available for versions greater than v8.0.
     if not changelog:
@@ -31,10 +39,10 @@ def parse_version_changes(from_version: str, to_version: str) -> list:
     # Parse the JSON file.
     # The techniques in the changelog are categorized (e.g. major change, addition, deletion, minor change, ...).
     # Iterate through all categories.
-    for category in modified_techniques:
+    for change_category in modified_techniques:
 
         # Iterate through all techniques in the category.
-        for technique in modified_techniques[category]:
+        for technique in modified_techniques[change_category]:
 
             # The text in detailed_diff is JSON formatted, but is interpreted by python as a string, so we have to parse it manually (no idea why).
             json_diff = json.loads(technique.get("detailed_diff", "{}"))
@@ -44,27 +52,62 @@ def parse_version_changes(from_version: str, to_version: str) -> list:
             old_description = glom(json_diff, "values_changed.root['description'].old_value", default="N/A")
             new_description = glom(json_diff, "values_changed.root['description'].new_value", default=technique.get("description"))
 
+            # Get the platforms of the technique in JSON format.
+            platforms = json.dumps(technique.get("x_mitre_platforms"))
+
             # All other changes (except the description) will be stored as JSON in the database, since they are very dynamic.
             json_diff.get("values_changed", {}).pop("root['description']", None)
             other_changes = json.dumps(json_diff)
 
-            # Fill out all necessary fields of the MITREChange object.
-            change = MITREChange(
-                mitre_id = technique["external_references"][0]["external_id"],
-                url = technique["external_references"][0]["url"],
-                technique_name = technique.get("name"),
-                old_description = old_description,
-                new_description = new_description,
-                other_changes = other_changes,
-                change_category = category,
-                from_version = from_version,
-                to_version = to_version
-            )
+            # Category = MITRE Tactic
+            # A MITRE technique can belong to multiple tactics.
+            # Each combination of tactic and technique has it's own entry in the database.
+            # Depending on the tactic the scores can be different.
+            categories = technique.get("kill_chain_phases")
 
-            result.append(change)
+            # Iterate over all references and extract the 'url' and 'external_id' (-> MITRE ID).
+            # Default value is None.
+            url = next((ref.get("url") for ref in technique.get("external_references") if ref.get("source_name") == "mitre-attack"), "")
+
+            mitre_id = next((ref.get("external_id") for ref in technique.get("external_references")if ref.get("source_name") == "mitre-attack"), "")
+
+            # If sub-technique, get the name of the parent technique (-> sub_category).
+            if "." in mitre_id:
+                parent_id = mitre_id.split('.')[0]
+                parent_technique = next((object for object in attack_data.get("objects") if object.get("type") == "attack-pattern" and any(ref.get("external_id") == parent_id for ref in object.get("external_references", []))), {})
+                sub_category = parent_technique.get("name", "")
+                technique_name = technique.get("name")
+                print(f"MITREID: {mitre_id} -- ParentID: {parent_id} -- Sub-Category: {sub_category} -- Technique: {technique_name}")
+            
+            # If not a sub-technique, then the technique name is the sub-category.
+            else:
+                sub_category = technique.get("name")
+                technique_name = ""
+
+
+            for c in categories:
+                # Get the name of the tactic/category.
+                category = c.get("phase_name")
+
+                # Fill out all necessary fields of the MITREChange object.
+                change = MITREChange(
+                    mitre_id = mitre_id,
+                    url = url,
+                    technique = technique_name,
+                    category = category,
+                    sub_category = sub_category,
+                    old_description = old_description,
+                    new_description = new_description,
+                    other_changes = other_changes,
+                    change_category = change_category,
+                    from_version = from_version,
+                    to_version = to_version,
+                    platforms = platforms
+                )
+
+                result.append(change)
     
     return result
-
 
 
 '''
@@ -74,7 +117,6 @@ def parse_version_changes(from_version: str, to_version: str) -> list:
 =====================================================================================
 '''
 def get_versions_db(current_version: str, db: Session) -> tuple[MITREVersion, MITREVersion]:
-    
     # Get current version from DB.
     current_version = db.scalar(select(MITREVersion).where(MITREVersion.name == current_version))
 
@@ -125,11 +167,119 @@ def upgrade_exists(from_version: str, db: Session) -> bool:
 '''
 def get_changes(from_version: str, to_version: str, db: Session):
     return db.scalars(
-        select(MITREChange).where(
+        select(MITREChange) \
+        .where(
             (MITREChange.from_version == from_version) &
             (MITREChange.to_version == to_version)
+        ) \
+        .order_by(
+            asc(MITREChange.category),
+            asc(MITREChange.sub_category),
+            asc(MITREChange.technique)
         )
     ).all()
+
+
+
+# get one particular change from db.
+def get_change(db: Session, from_version: str, to_version: str, category: str, mitre_id: str) -> MITREChange:
+    return db.scalar(
+        select(MITREChange) \
+        .where(
+            (MITREChange.from_version == from_version) &
+            (MITREChange.to_version == to_version) &
+            (MITREChange.category == category) &
+            (MITREChange.mitre_id == mitre_id)
+        )
+    )
+
+
+
+def get_previous_change(db: Session, current_change: MITREChange, filter: str) -> MITREChange:
+    # If filer is "All" or there is no filter set, check all changes.
+    if filter == "All" or not filter:
+        query = select(MITREChange) \
+        .where(
+            (MITREChange.from_version == current_change.from_version) &
+            (MITREChange.to_version == current_change.to_version) &
+            (MITREChange.change_category == current_change.change_category)
+        ) \
+        .order_by(
+            asc(MITREChange.category),
+            asc(MITREChange.sub_category),
+            asc(MITREChange.technique)
+        )
+    
+    # If filter is "Done", "In Progress" or "Not Done", only get the specific records from DB.
+    else:
+        query = select(MITREChange) \
+        .where(
+            (MITREChange.from_version == current_change.from_version) &
+            (MITREChange.to_version == current_change.to_version) &
+            (MITREChange.change_category == current_change.change_category) &
+            (MITREChange.status == filter)
+        ) \
+        .order_by(
+            asc(MITREChange.category),
+            asc(MITREChange.sub_category),
+            asc(MITREChange.technique)
+        )
+
+    # Determine index of current change and subtract 1 to get previous change.
+    results = db.scalars(query).all()
+    index = next(
+        i for i, r in enumerate(results)
+        if r.mitre_id == current_change.mitre_id
+        and r.category == current_change.category
+    )
+
+    # If index is 0, there is no previous change, so return 'None'.
+    # Else return the previous change (-> 'results[index - 1]').
+    return results[index - 1] if index != 0 else None
+
+
+
+
+
+def get_next_change(db: Session, current_change: MITREChange, filter: str) -> MITREChange:
+    if filter == "All" or not filter:
+        query = select(MITREChange) \
+        .where(
+            (MITREChange.from_version == current_change.from_version) &
+            (MITREChange.to_version == current_change.to_version) &
+            (MITREChange.change_category == current_change.change_category)
+        ) \
+        .order_by(
+            asc(MITREChange.category),
+            asc(MITREChange.sub_category),
+            asc(MITREChange.technique)
+        )
+    
+    else:
+        query = select(MITREChange) \
+        .where(
+            (MITREChange.from_version == current_change.from_version) &
+            (MITREChange.to_version == current_change.to_version) &
+            (MITREChange.change_category == current_change.change_category) &
+            (MITREChange.status == filter)
+        ) \
+        .order_by(
+            asc(MITREChange.category),
+            asc(MITREChange.sub_category),
+            asc(MITREChange.technique)
+        )
+    
+    # Determine index of current change and add 1 to get next change.
+    results = db.scalars(query).all()
+    index = next(
+        i for i, r in enumerate(results)
+        if r.mitre_id == current_change.mitre_id
+        and r.category == current_change.category
+    )
+    
+    # If 'current_change' is the last one in the results, return None.
+    # Else, return the next change (-> 'results[index + 1]').
+    return results[index + 1] if (index + 1) != len(results) else None
 
 
 
